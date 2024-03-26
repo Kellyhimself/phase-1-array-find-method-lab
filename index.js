3279bd3d3c343a1f7f5163c0ef7263b9b44ff826
Kitui Kelly <kituikelly@gmail.com>
2024-03-26 16:57:09 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(Array) {
+  const win = Array.find((object) => object.result === "W");
+  return win ? win.year : undefined;
+}
+/* this function takes an array of objects as an argument. uses the find() 
+method to iterate over the individual objects in the array and returns the 
+first elemnt/object whose result property is "W". game    */
